acb7cf226fb5a160d2c1bd26f08911d9d465d492
rajeshsantha <rajesh.santha29@gmail.com>
2023-05-03 23:16:50 +0530

    modify find elements in lists

--- dataStructures/findItems.py
+++ dataStructures/findItems.py
@@ -1,10 +1,12 @@
-letters = ["a", "b", "c", "d"]
+letters = ["a", "b", "c", "d", "b", "c", "b", "c"]
 print(letters.index("b"))  # 1
 # print(letters.index("x"))  # ValueError: 'x' is not in list
 
 # To prevent the above error, see if the item is there then get the index of the value
-inputValue = "x"
+inputValue = "b"
 if inputValue in letters:
     print(letters.index(inputValue))  # 1
+    numberOfOccurancesInList = letters.count(inputValue)
+    print(f"{inputValue} occured {numberOfOccurancesInList} times in the list")
 else:
     print(f"\" {inputValue} \" doesn't exist in the list")
