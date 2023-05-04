d18487957d8f1bb7d3612b04fea5b36af94c155c
rajeshsantha <rajesh.santha29@gmail.com>
2023-05-04 19:56:03 +0530

    add exercise

--- dataStructures/ExerciseOnDS.py
+++ dataStructures/ExerciseOnDS.py
@@ -1,33 +1,33 @@
 # get Most repeated character in this below text
 sentence = "This is a common interview question"
 
+
+# APPROACH : BY USING DICTIONARY
 char_frequency = {}
 for i in sentence:
     if i in char_frequency:
         # update the count
-        char_frequency.update({i: char_frequency[i]+1})
+        char_frequency[i] += 1
+        # YOU CAN ALSO DO THIS BY
+        # char_frequency[i] = char_frequency[i] + 1
+        # char_frequency.update({i: char_frequency[i]+1}) // same thing.
     else:
         # insert the entry with value 1 , as it is first time occuring
         char_frequency[i] = 1
 
-print(char_frequency)
+# print(char_frequency)
+# print("************")
 
-for key, value in char_frequency.items():
-    print(key, value)
-# T 1
-# h 1
-# i 5
-# s 3
-#   5
-# a 1
-# c 1
-# o 3
-# m 2
-# n 3
-# t 2
-# e 3
-# r 1
-# v 1
-# w 1
-# q 1
-# u 1
+newSortedList = sorted(list(char_frequency.items()),
+                       key=lambda x: x[1], reverse=True)
+# print(newSortedList)
+print(f"****The most repeated character is : {newSortedList[0]} ")
+
+if newSortedList:
+    mostRepeatedCharacter = newSortedList[0][0]
+else:
+    print("input sentence is empty")
+
+
+print(f"The most repeated character is : {mostRepeatedCharacter} ")
+# The most repeated character is : i
