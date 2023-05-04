f8ca75e43490df3885c03c8822ce45e63ba2b7de
rajeshsantha <rajesh.santha29@gmail.com>
2023-05-04 22:51:35 +0530

    add exception01

---
+++ exceptions/exceptions01.py
@@ -0,0 +1,9 @@
+name = input("Name: ")
+age = int(input("Age: "))
+print("hi! ", name, ":  your age is ", age)
+# Name: rajesh
+# Age: sdjs
+# Traceback (most recent call last):
+#   File "/Users/rajeshsantha/vscode/PythonPracticeSnippets/exceptions/exceptions01.py", line 4, in <module>
+#     age = int(input("Age: "))
+# ValueError: invalid literal for int() with base 10: 'sdjs'
