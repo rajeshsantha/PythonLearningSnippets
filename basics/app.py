307f8cff2177594d1d2a8170eabea6518bd7feb8
rajeshsantha <rajesh.santha29@gmail.com>
2024-04-12 22:18:16 +0530

    update app.py

--- basics/app.py
+++ basics/app.py
@@ -1,8 +1,8 @@
-pullstop = "."
-space = " "
-input = 5
+FULLSTOP = "."
+SPACE = " "
+INPUT = 5
 for number in range(1, 11):
-    print(f"{input} * {number}{(3-len(str(number)))*space}= {(2-len(str(input * number)))*space}{input * number}")
+    print(f"{INPUT} * {number}{(3 - len(str(number))) * SPACE}= {(2 - len(str(INPUT * number))) * SPACE}{INPUT * number}")
 
 #        f"attempt number {number+1}{(3-len(str(number+1)))*space}{(number+1)*pullstop}")
 # 5 * 1 = 5
